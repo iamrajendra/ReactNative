e6efd037ddf38d585893d7396cd0fd0eafd30e81
Raj Verma <rajendrarajaramv@gmail.com>
2021-12-12 18:06:21 +0530

    Get the India Parliament list

--- screen/Home.js
+++ screen/Home.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Button, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Text,SafeAreaView,StyleSheet,StatusBar } from 'react-native';
+
 
 const HomeScreen = ({ navigation }) => {
+
+const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://raw.githubusercontent.com/iamrajendra/services/master/seats.json');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
-      }
-    />
+            <SafeAreaView style={styles.container}>
+      {isLoading ? <ActivityIndicator/> : (
+
+        <FlatList
+          data={data}
+          keyExtractor={({ item }, index) => item}
+          renderItem={({ item }) => (
+            <Text
+style  = {styles.item}
+            >{item.state}, {item.seats}
+
+            </Text>
+          )}
+        />
+      )}
+    </SafeAreaView>
   );
+
+
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+  },
+  item: {
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 5,
+  },
+  title: {
+    fontSize: 10,
+  },
+});
+
 export {HomeScreen};
